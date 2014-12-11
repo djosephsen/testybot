32ee57fdf67732660869c6569933467e600c4cd3
Dave Josephsen <dave@skeptech.org>
2014-12-11 15:56:44 -0600

    choring

--- Godeps/_workspace/src/github.com/djosephsen/bothandlers/choreCommands.go
+++ Godeps/_workspace/src/github.com/djosephsen/bothandlers/choreCommands.go
@@ -13,6 +13,7 @@ var ListChores = &hal.Handler{
 		if len(res.Robot.Chores) == 0{
 			reply=`No chores have been registered (sorry?)`
 		}else{
+			reply=`Name  :small_blue_diamond:  Schedule  :small_blue_diamond:  Next Run  :small_blue_diamond: Current State`
 			for _,c := range res.Robot.Chores{
 				reply = fmt.Sprintf("%s\n%s:small_blue_diamond:%s:small_blue_diamond:%s:small_blue_diamond:%s",reply,c.Name, c.Schedule, c.Next, c.State)
 			}
