9371fcff5458ad53b8b774f7e28e7f64f800c406
Dave Josephsen <dave@skeptech.org>
2014-12-11 23:30:03 -0600

    re-enable all chores

--- Godeps/_workspace/src/github.com/djosephsen/bothandlers/secondtest.go
+++ Godeps/_workspace/src/github.com/djosephsen/bothandlers/secondtest.go
@@ -7,7 +7,7 @@ import (
 var Secondtest = &hal.Chore{
 	Name:  `Second Test`,
 	Sched: `0 * * * * * *`,
-	Room: `C02JM295Z`,
+	Room: `C031P7M3E`,
 	Run: func(res *hal.Response) error {
 		return res.Send(`successful test!`)
 	},
